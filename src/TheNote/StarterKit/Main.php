60411728291496e934b89335a38fe615912722e5
TobiasDev <tobiasdevde@gmail.com>
2019-10-10 18:02:49 +0200

    New Config Layout etc

--- src/TheNote/StarterKit/Main.php
+++ src/TheNote/StarterKit/Main.php
@@ -2,6 +2,7 @@
 
 namespace TheNote\StarterKit;
 
+use pocketmine\item\ItemFactory;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\Listener;
@@ -11,32 +12,30 @@ use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener
 {
-    /** @var Config */
-    private $cfg;
+
+    private $items = [];
 
     public function onEnable()
     {
         $this->saveResource("config.yml");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
+        $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        $this->items = (array) $cfg->get("Slots");
+
     }
 
+    /**
+     * @param PlayerJoinEvent $event
+     * @PRIORITY LOWEST
+     */
     public function onJoin(PlayerJoinEvent $event)
     {
         $player = $event->getPlayer();
 
-        $i = $this->cfg->getAll();
-        if (!$player->hasPlayedBefore()) {
-            $player->getInventory()->setItem(1, Item::get($i["Slot1"]["id"], $i["Slot1"]["damage"], $i["Slot1"]["count"]));
-            $player->getInventory()->setItem(2, Item::get($i["Slot2"]["id"], $i["Slot2"]["damage"], $i["Slot2"]["count"]));
-            $player->getInventory()->setItem(3, Item::get($i["Slot3"]["id"], $i["Slot3"]["damage"], $i["Slot3"]["count"]));
-            $player->getInventory()->setItem(4, Item::get($i["Slot4"]["id"], $i["Slot4"]["damage"], $i["Slot4"]["count"]));
-            $player->getInventory()->setItem(5, Item::get($i["Slot5"]["id"], $i["Slot5"]["damage"], $i["Slot5"]["count"]));
-            $player->getInventory()->setItem(6, Item::get($i["Slot6"]["id"], $i["Slot6"]["damage"], $i["Slot6"]["count"]));
-            $player->getInventory()->setItem(7, Item::get($i["Slot7"]["id"], $i["Slot7"]["damage"], $i["Slot7"]["count"]));
-            $player->getInventory()->setItem(8, Item::get($i["Slot8"]["id"], $i["Slot8"]["damage"], $i["Slot8"]["count"]));
-            $player->getInventory()->setItem(9, Item::get($i["Slot9"]["id"], $i["Slot9"]["damage"], $i["Slot9"]["count"]));
-
+        if(!$player->hasPlayedBefore()){
+            foreach($this->items as $item){
+                $player->getInventory()->setItem($item["slot"], ItemFactory::get($item["id"], $item["damage"], $item["count"]));
+            }
         }
     }
 }
