cb12cc0483c40f46c044c9bd405fcd78a8ba8b0f
TheNote <38653750+note3crafter@users.noreply.github.com>
2019-12-20 17:48:44 +0100

    Update Main.php

--- src/TheNote/StarterKit/Main.php
+++ src/TheNote/StarterKit/Main.php
@@ -22,15 +22,15 @@ class Main extends PluginBase implements Listener {
             $this->cfg = new Config($this->getDataFolder()."config.yml", Config::YAML, array());
             $i = $this->cfg->getAll();
             if(!$player->hasPlayedBefore()) {
-                $player->getInventory()->setItem(1, Item::get($i["Slot1"]["id"], $i["Slot1"]["damage"], $i["Slot1"]["count"]));
-                $player->getInventory()->setItem(2, Item::get($i["Slot2"]["id"], $i["Slot2"]["damage"], $i["Slot2"]["count"]));
-                $player->getInventory()->setItem(3, Item::get($i["Slot3"]["id"], $i["Slot3"]["damage"], $i["Slot3"]["count"]));
-                $player->getInventory()->setItem(4, Item::get($i["Slot4"]["id"], $i["Slot4"]["damage"], $i["Slot4"]["count"]));
-                $player->getInventory()->setItem(5, Item::get($i["Slot5"]["id"], $i["Slot5"]["damage"], $i["Slot5"]["count"]));
-                $player->getInventory()->setItem(6, Item::get($i["Slot6"]["id"], $i["Slot6"]["damage"], $i["Slot6"]["count"]));
-                $player->getInventory()->setItem(7, Item::get($i["Slot7"]["id"], $i["Slot7"]["damage"], $i["Slot7"]["count"]));
-                $player->getInventory()->setItem(8, Item::get($i["Slot8"]["id"], $i["Slot8"]["damage"], $i["Slot8"]["count"]));
-                $player->getInventory()->setItem(9, Item::get($i["Slot9"]["id"], $i["Slot9"]["damage"], $i["Slot9"]["count"]));
+                $player->getInventory()->setItem(0, Item::get($i["Slot1"]["id"], $i["Slot1"]["damage"], $i["Slot1"]["count"]));
+                $player->getInventory()->setItem(1, Item::get($i["Slot2"]["id"], $i["Slot2"]["damage"], $i["Slot2"]["count"]));
+                $player->getInventory()->setItem(2, Item::get($i["Slot3"]["id"], $i["Slot3"]["damage"], $i["Slot3"]["count"]));
+                $player->getInventory()->setItem(3, Item::get($i["Slot4"]["id"], $i["Slot4"]["damage"], $i["Slot4"]["count"]));
+                $player->getInventory()->setItem(4, Item::get($i["Slot5"]["id"], $i["Slot5"]["damage"], $i["Slot5"]["count"]));
+                $player->getInventory()->setItem(5, Item::get($i["Slot6"]["id"], $i["Slot6"]["damage"], $i["Slot6"]["count"]));
+                $player->getInventory()->setItem(6, Item::get($i["Slot7"]["id"], $i["Slot7"]["damage"], $i["Slot7"]["count"]));
+                $player->getInventory()->setItem(7, Item::get($i["Slot8"]["id"], $i["Slot8"]["damage"], $i["Slot8"]["count"]));
+                $player->getInventory()->setItem(8, Item::get($i["Slot9"]["id"], $i["Slot9"]["damage"], $i["Slot9"]["count"]));
 
             }
         }
