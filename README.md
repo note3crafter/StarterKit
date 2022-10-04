51c3d9520728628d15174928b9a30e7d37547883
TheNote <38653750+note3crafter@users.noreply.github.com>
2022-10-04 20:10:18 +0200

    PocketMine API 5.0.0 Beta4 Update

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
-# StarterKit
+# StarterKit for Pocketmine API 5.0.0
 A StarterKit plugin where you can insert your starter items in the Config that a new player can get
 In the Config you can easily change the items yourself
 
 # Config
 
-Welcome to StarterKit 3.0.0 by TheNote
+Welcome to StarterKit 4.0.0 by TheNote
 
 You Can now setup more Features!
 
  * - slot: <- Set the Slot wo are the Item Place (0-35)
  *   id: <- set the ItemID of the Item
- *   damage: <- set the damage of the Item
  *   count: <- set the amount of the Item
  *   name: <- set a Customname for the Item
  *   lore: <- set the Lore of the Item
@@ -19,5 +18,5 @@ You Can now setup more Features!
 
 have fun with the plugin and make a issue for Proplems on GitHub
 Plugin mate by TheNote
-[![Discord](https://img.shields.io/discord/427472879072968714.svg?style=flat-square&label=discord&colorB=7289da)](https://discord.gg/XwXKuvy) |<br>
+[![Discord](https://img.shields.io/discord/427472879072968714.svg?style=flat-square&label=discord&colorB=7289da)](https://discord.gg/ju2gGzq) |<br>
 
--- src/TheNote/StarterKit/Main.php
+++ src/TheNote/StarterKit/Main.php
@@ -2,7 +2,7 @@
 
 namespace TheNote\StarterKit;
 
-use pocketmine\item\ItemFactory;
+use pocketmine\item\StringToItemParser;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\Listener;
@@ -18,42 +18,42 @@ class Main extends PluginBase implements Listener{
 	public function onJoin(PlayerJoinEvent $event){
 		$player = $event->getPlayer();
 		$ainv = $player->getArmorInventory();
-
 		$cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML, []);
 		if(!$player->hasPlayedBefore()){
-			if($cfg->get("Inventory", false)){
-				foreach($cfg->get("Slots", []) as $item){
-					$result = ItemFactory::getInstance()->get($item["id"], $item["damage"], $item["count"]);
-					$result->setCustomName($item["name"]);
-					$result->setLore([$item["lore"]]);
-					$player->getInventory()->setItem($item["slot"], $result);
-				}
-			}
-			if($cfg->get("Armor", false)){
-				$data = $cfg->get("helm");
-				$item = ItemFactory::getInstance()->get($data["id"]);
-				$item->setCustomName($data["name"]);
-				$item->setLore([$data["lore"]]);
-				$ainv->setHelmet($item);
+            if ($cfg->get("Inventory") === true) {
+                foreach ($cfg->get("Slots", []) as $slot) {
+                    $item = StringToItemParser::getInstance()->parse($slot["id"]);
+                    $item->setCount($slot["count"]);
+                    $item->setCustomName($slot["name"]);
+                    $item->setLore([$slot["lore"]]);
+                    $player->getInventory()->addItem($item);
+                }
+            }
+            if ($cfg->get("Armor") === true) {
+                $data = $cfg->get("helm");
+                $item = StringToItemParser::getInstance()->parse($data["id"]);
+                $item->setCustomName($data["name"]);
+                $item->setLore([$data["lore"]]);
+                $ainv->setHelmet($item);
 
-				$data = $cfg->get("chest");
-				$item = ItemFactory::getInstance()->get($data["id"]);
-				$item->setCustomName($data["name"]);
-				$item->setLore([$data["lore"]]);
-				$ainv->setChestplate($item);
+                $data = $cfg->get("chest");
+                $item = StringToItemParser::getInstance()->parse($data["id"]);
+                $item->setCustomName($data["name"]);
+                $item->setLore([$data["lore"]]);
+                $ainv->setChestplate($item);
 
-				$data = $cfg->get("leggins");
-				$item = ItemFactory::getInstance()->get($data["id"]);
-				$item->setCustomName($data["name"]);
-				$item->setLore([$data["lore"]]);
-				$ainv->setLeggings($item);
+                $data = $cfg->get("leggins");
+                $item = StringToItemParser::getInstance()->parse($data["id"]);
+                $item->setCustomName($data["name"]);
+                $item->setLore([$data["lore"]]);
+                $ainv->setLeggings($item);
 
-				$data = $cfg->get("boots");
-				$item = ItemFactory::getInstance()->get($data["id"]);
-				$item->setCustomName($data["name"]);
-				$item->setLore([$data["lore"]]);
-				$ainv->setBoots($item);
-			}
+                $data = $cfg->get("boots");
+                $item = StringToItemParser::getInstance()->parse($data["id"]);
+                $item->setCustomName($data["name"]);
+                $item->setLore([$data["lore"]]);
+                $ainv->setBoots($item);
+            }
 		}
 	}
 }
