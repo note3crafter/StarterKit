6bce7c58c993679cfce39f21fed174121354b2c4
TheNote <38653750+note3crafter@users.noreply.github.com>
2023-06-04 14:21:58 +0200

    API 5 Update

--- src/TheNote/StarterKit/Main.php
+++ src/TheNote/StarterKit/Main.php
@@ -8,21 +8,24 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\Listener;
 use pocketmine\utils\Config;
 
-class Main extends PluginBase implements Listener{
+class Main extends PluginBase implements Listener
+{
 
-	public function onEnable() : void{
-		$this->saveResource("config.yml");
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-	}
+    public function onEnable(): void
+    {
+        $this->saveResource("config.yml");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
 
-	public function onJoin(PlayerJoinEvent $event){
-		$player = $event->getPlayer();
-		$ainv = $player->getArmorInventory();
-		$cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML, []);
-		if(!$player->hasPlayedBefore()){
+    public function onJoin(PlayerJoinEvent $event)
+    {
+        $player = $event->getPlayer();
+        $ainv = $player->getArmorInventory();
+        $cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML, []);
+        if (!$player->hasPlayedBefore()) {
             if ($cfg->get("Inventory") === true) {
                 foreach ($cfg->get("Slots", []) as $slot) {
-                    $item = StringToItemParser::getInstance()->parse($slot["id"]);
+                    $item = StringToItemParser::getInstance()->parse($slot["item"]);
                     $item->setCount($slot["count"]);
                     $item->setCustomName($slot["name"]);
                     $item->setLore([$slot["lore"]]);
@@ -31,29 +34,29 @@ class Main extends PluginBase implements Listener{
             }
             if ($cfg->get("Armor") === true) {
                 $data = $cfg->get("helm");
-                $item = StringToItemParser::getInstance()->parse($data["id"]);
+                $item = StringToItemParser::getInstance()->parse($data["item"]);
                 $item->setCustomName($data["name"]);
                 $item->setLore([$data["lore"]]);
                 $ainv->setHelmet($item);
 
                 $data = $cfg->get("chest");
-                $item = StringToItemParser::getInstance()->parse($data["id"]);
+                $item = StringToItemParser::getInstance()->parse($data["item"]);
                 $item->setCustomName($data["name"]);
                 $item->setLore([$data["lore"]]);
                 $ainv->setChestplate($item);
 
                 $data = $cfg->get("leggins");
-                $item = StringToItemParser::getInstance()->parse($data["id"]);
+                $item = StringToItemParser::getInstance()->parse($data["item"]);
                 $item->setCustomName($data["name"]);
                 $item->setLore([$data["lore"]]);
                 $ainv->setLeggings($item);
 
                 $data = $cfg->get("boots");
-                $item = StringToItemParser::getInstance()->parse($data["id"]);
+                $item = StringToItemParser::getInstance()->parse($data["item"]);
                 $item->setCustomName($data["name"]);
                 $item->setLore([$data["lore"]]);
                 $ainv->setBoots($item);
             }
-		}
-	}
+        }
+    }
 }
